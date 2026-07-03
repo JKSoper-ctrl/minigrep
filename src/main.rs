e33d47c395ebbddabc1361b1b8a80ffbb1fba46a
JKSoper-ctrl <jamessoper300@gmail.com>
2026-07-03 22:01:38 +0100

    feat: add unimplemented search function in new 'lib.rs'

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-//use std::env;
-//use std::fs;
-//use std::process;
-
 use std::{env, fs, process, error::Error};
+use minigrep::search;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,25 +39,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
